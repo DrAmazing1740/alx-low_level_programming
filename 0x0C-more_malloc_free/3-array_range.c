2831022fc1a20c0ccf6f54db4ef9d15009abfa9d
DrAmazing1740 <36157660+DrAmazing1740@users.noreply.github.com>
2023-03-17 00:42:01 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *array_range - creates an array of integers
+ * *array_range - will create an array of integers
  * @min: minimum range of values stored
  * @max: maximum range of values stored and number of elements
  *
