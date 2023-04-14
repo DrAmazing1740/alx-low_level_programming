780a40e694d3dc8be790c40da601f0d75351c5e6
DrAmazing1740 <36157660+DrAmazing1740@users.noreply.github.com>
2023-04-15 00:14:54 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,13 @@
+#include "main.h"
+/**
+ * append_text_to_file - Append text at the tail of the file.
+ * @filename: Point to the name of the file.
+ * @text_content: String to add to the tail of a file.
+ *
+ * Return: Function failure or filename is NULL - -1.
+ *         If the file is nonexistent the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
