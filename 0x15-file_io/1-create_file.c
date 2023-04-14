e36cc561aa581563dda5dd238e40593b2950491d
DrAmazing1740 <36157660+DrAmazing1740@users.noreply.github.com>
2023-04-15 00:02:36 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - creates file
  * @filename: filename to add
- * @text_content: content 
+ * @text_content: content
  * Return: int value
  */
 int create_file(const char *filename, char *text_content)
